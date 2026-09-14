653dc942b17ed810f58abfc357e31f74a9feff41
zxz4255 <zxz4067@outlook.com>
2026-09-15 06:52:40 +0800

    Remove analytics from ViewModelModule
    Removed analytics dependency from ViewModelModule.

--- app/src/main/java/me/rerere/rikkahub/di/ViewModelModule.kt
+++ app/src/main/java/me/rerere/rikkahub/di/ViewModelModule.kt
@@ -33,7 +33,6 @@ val viewModelModule = module {
             conversationRepo = get(),
             chatService = get(),
             updateChecker = get(),
-            analytics = get(),
             filesManager = get(),
             favoriteRepository = get(),
         )
