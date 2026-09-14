a32025972afe18beb78473a50ac35c55e44d3df6
zxz4255 <zxz4067@outlook.com>
2026-09-15 06:51:57 +0800

    Remove Firebase crashlytics and analytics dependencies
    Removed Firebase crashlytics and analytics from AppModule.

--- app/src/main/java/me/rerere/rikkahub/di/AppModule.kt
+++ app/src/main/java/me/rerere/rikkahub/di/AppModule.kt
@@ -1,8 +1,5 @@
 package me.rerere.rikkahub.di
 
-import com.google.firebase.Firebase
-import com.google.firebase.analytics.analytics
-import com.google.firebase.crashlytics.crashlytics
 import kotlinx.serialization.json.Json
 import me.rerere.rikkahub.AppScope
 import me.rerere.rikkahub.data.ai.tools.local.LocalTools
@@ -48,14 +45,6 @@ val appModule = module {
 
     single {
         TTSManager(get())
-    }
-
-    single {
-        Firebase.crashlytics
-    }
-
-    single {
-        Firebase.analytics
     }
 
     single {
